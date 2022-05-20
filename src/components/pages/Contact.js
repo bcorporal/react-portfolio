4dbaa2cfadae51a177527d45c5f2bfd3685ebd34
bcorporal <bcorporal@gmail.com>
2022-05-20 14:22:27 -0400

    update contact form to work

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import '../../styles/contact.css';
+import { useForm, ValidationError } from '@formspree/react';
 
-export default function Contact() {
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mjvljnkl");
+  if (state.succeeded) {
+      return <p>Thank Your For Your Submission</p>;
+  }
   return (
-    <section id="contact">
-  <div>
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="row">
-          <div class="col-sm-12 col-sm-offset-2">
-            <div>
-              <h3>CONTACT ME</h3>
-            </div>
-            
-            <p>You can always email me at <span><a href="mailto:bc@bcorporal.dev">bc@bcorporal.dev</a></span></p>
-            <p>View my <span><a href="https://www.linkedin.com/in/brandon-corporal-65742822b">LinkedIn</a></span></p>
-            <p>View my <span><a href="https://github.com/bcorporal">Github</a></span></p>
-            <form method="post" data-form-title="CONTACT US">
-              <input type="hidden" data-form-email="true"></input>
-              <div class="form-group">
-                <input type="text" class="form-control" name="name" required="" placeholder="Name*" data-form-field="Name"></input>
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control" name="email" required="" placeholder="Email*" data-form-field="Email"></input>
-              </div>
-              <div class="form-group">
-                <input type="tel" class="form-control" name="phone" placeholder="Phone" data-form-field="Phone"></input>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" placeholder="Message" rows="7" data-form-field="Message"></textarea>
-              </div>
-              <div>
-                <button type="submit" class="btn btn-lg btn-danger">SEND</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+    <form onSubmit={handleSubmit}>
+    <h1 htmlFor="email">Contact Me</h1>
+    <h5>Please enter details below</h5>
+    <h3>Name</h3>
+    <input
+      id="nameInput"
+      type="name"
+      name="Full Name"
+      placeholder="Full Name.."
+    />
+    <h3>Email</h3>
+    <input id="email" type="email" name="email" placeholder="Email.." />
+    <ValidationError prefix="Email" field="email" errors={state.errors} />
+    <h3>Message</h3>
+    <textarea
+      id="message"
+      name="message"
+      placeholder="Enter Message Here.."
+    />
+    <ValidationError prefix="Message" field="message" errors={state.errors} />
+    <br></br>
+    <button type="submit" disabled={state.submitting}>
+      Submit
+    </button>
+  </form>
   );
 }
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
+
+
