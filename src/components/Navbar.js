59449064d8ec1ba1a21d07c9cb308e0c0452024b
bcorporal <bcorporal@gmail.com>
2022-06-04 20:33:20 -0400

    portfolio name change to projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ function Navbar({ currentPage, handlePageChange }) {
       onClick={() => handlePageChange('Portfolio')}
       className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
       >
-        Portfolio
+        Projects
       </a></li>
 
       <li className='nav-item'>
