808c3ed673805ebbeb7eefedebba746482065515
bcorporal <bcorporal@gmail.com>
2022-06-06 13:29:59 -0400

    pwa

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,9 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 export { default as About } from "./About"
 export { default as Blog } from "./Blog"
 export { default as Contact } from "./Contact"
 export { default as Home } from "./Home"
+
 
 ReactDOM.render(
     <React.StrictMode>
@@ -10,3 +12,9 @@ ReactDOM.render(
     document.getElementById('root')
   );
   reportWebVitals();  
+
+
+  // If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
